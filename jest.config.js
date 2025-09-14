dcfc4c338919d1d396a43343403e5cac6bca0dff
MatNaw77 <mateusz.nawrat7@gmail.com>
2025-09-14 18:28:32 +0700

    init app

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+};
