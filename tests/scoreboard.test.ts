93463a6e74515175f6ec0fb9f42475708b5eb516
MatNaw77 <mateusz.nawrat7@gmail.com>
2025-09-15 07:56:43 +0700

    test: add failing test for starting a match

---
+++ tests/scoreboard.test.ts
@@ -0,0 +1,19 @@
+import { Scoreboard } from '@/lib/Scoreboard';
+
+describe("Scoreboard tests", () => {
+    const scoreboard = new Scoreboard();
+
+    it("should start a new match Poland-Brazil with 0-0 score", () => {
+        scoreboard.startMatch("Poland", "Brazil");
+
+        const matches = scoreboard.getSummary();
+        const match = matches[0];
+        expect(matches.length).toBe(1);
+        expect(match).toMatchObject({
+            homeTeam: "Poland",
+            awayTeam: "Brazil",
+            homeScore: 0,
+            awayScore: 0,
+        });
+    });
+});
